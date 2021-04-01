fabfb2f1cd8c60de810f6bb4d2844eb800df3dea
Jung David Lee <jungdavidlee@gmail.com>
2021-03-31 18:30:29 -0700

    created routes in controllers and index.js

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+const commentRoutes = require("./api/comment-routes");
+const userRoutes = require("./api/user-routes");
+const postRoutes = require("./api/post-routes");
+
+const homeRoutes = require("./home-routes");
+
+router.use("/", homeRoutes);
+router.use("/api/comments", commentRoutes);
+router.use("/api/users", userRoutes);
+router.use("/api/posts", postRoutes);
+
+module.exports = router;
