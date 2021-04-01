b5b33931dfa8bdec28c29222e50f73338e4296cb
Jung David Lee <jungdavidlee@gmail.com>
2021-03-31 17:46:19 -0700

    added more consts in server.js

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
+sequelize.sync({force: false}).then(()=> {
+    app.listen(PORT, ()=> {
+        console.log(`Now listening on PORT: ${PORT}`);
+    })
+})
 
