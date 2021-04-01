30b360415f74b835da618145e72daacc4d83e1b8
Jung David Lee <jungdavidlee@gmail.com>
2021-03-31 19:56:59 -0700

    updated server.js to add controllers

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const controller = require('./controllers');
 const session = require('express-session');
 const sequelize = require('./config/connection');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
@@ -15,10 +14,18 @@ const sess = {
     store: new SequelizeStore({ db: sequelize })
 };
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
+
+app.use('/', controller);
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 sequelize.sync({force: false}).then(()=> {
     app.listen(PORT, ()=> {
